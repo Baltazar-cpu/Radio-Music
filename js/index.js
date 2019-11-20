cc54897683ef48d2433530496e7d00c2e5390641
Baltazar-cpu <remen201388evo@gmail.com>
2019-11-20 11:44:48 +0000

    greedings

--- js/index.js
+++ js/index.js
@@ -6,3 +6,21 @@ $(document).ready(function () {
         $(this).parents('body').find('.myAudio').trigger('pause');
   });
 });
+
+function startTime() {
+    let tm=new Date();
+    let h=tm.getHours();
+    let m=tm.getMinutes();
+    let s=tm.getSeconds();
+        m=checkTime(m);
+        s=checkTime(s);
+        document.getElementById('txt').innerHTML=h+":"+m+":"+s;
+        t=setTimeout('startTime()',500);
+}
+
+function checkTime(i) {
+    if (i<10) {
+        i="0" + i;
+    }
+    return i;
+}
